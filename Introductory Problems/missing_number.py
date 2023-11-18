ec11742cfe7eb0a97a609226809f117d5d0371f6
ROHIT SHARMA <43227329+devRawnie@users.noreply.github.com>
2023-11-18 22:28:47 +0530

    missing number

--- Introductory Problems/missing_number.py
+++ Introductory Problems/missing_number.py
@@ -4,9 +4,6 @@ numbers = list(map(int, input().split(" ")))
 all_numbers = set([i for i in range(1, n+1)])
 
 for n in numbers:
-    if n not in all_numbers:
-        print(n)
-        break
-    else:
-        all_numbers.remove(n)
-    
+    all_numbers.remove(n)
+
+print(*a)
