f77cd164b6bb9ce66d610100da81fa0f51b87d7a
ROHIT SHARMA <43227329+devRawnie@users.noreply.github.com>
2023-11-18 23:14:37 +0530

    permutation

--- Introductory Problems/permutations.py
+++ Introductory Problems/permutations.py
@@ -3,16 +3,16 @@ n = int(input())
 if n == 1:
     print(1)
 
-elif n < 5:
+elif n < 4:
     print("NO SOLUTION")
 
 else:
-    i = 1
+    i = 2
     while i <= n:
         print(i, end=" ")
         i += 2
 
-    i = 2
+    i = 1
     while i <= n:
         print(i, end=" ")
         i += 2
