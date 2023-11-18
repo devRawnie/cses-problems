24693dad6f86cab4cf52be438c64361cd9f822ee
ROHIT SHARMA <43227329+devRawnie@users.noreply.github.com>
2023-11-18 22:34:13 +0530

    missing number

--- Introductory Problems/missing_number.py
+++ Introductory Problems/missing_number.py
@@ -1,9 +1,15 @@
 n = int(input())
-numbers = list(map(int, input().split(" ")))
+numbers_sum = sum(list(map(int, input().split(" "))))
 
+"""
 all_numbers = set([i for i in range(1, n+1)])
 
 for n in numbers:
     all_numbers.remove(n)
 
 print(*all_numbers)
+"""
+
+expected_sum = (n*(n+1))//2
+total_sum = sum(numbers)
+print(expected_sum - total_sum)
